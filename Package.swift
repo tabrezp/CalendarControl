ae31529a8ea773d25d78523fee8c332f5a130b05
Tabrez <tabrezpasha@outlook.com>
2022-10-09 18:45:30 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -37,12 +37,18 @@ import PackageDescription
 
 let package = Package(
   name: "CalendarControl",
-  platforms: [.iOS(.v14)],
-  products: [.library(name: "CalendarControl",
-                      targets: ["CalendarControl"])],
+  platforms: [
+    .iOS(.v14), .tvOS(.v14)
+  ],
+  products: [
+    .library(
+      name: "CalendarControl",
+      targets: ["CalendarControl"]),
+  ],
   targets: [
-  .binaryTarget(
-    name: "CalendarControl",
-    path: "./Sources/CalendarControl.xcframework")
+    .binaryTarget(
+      name: "CalendarControl",
+      path: "./Sources/CalendarControl.xcframework"
+    )
   ]
 )
